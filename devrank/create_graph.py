8e8f2b8f1eead779e74e970ea1fd0b2f00ae606b
Maxime Lovino <maximelovino@gmail.com>
2019-06-10 14:25:54 +0200

    Refactored mining in single file with CLI arguments
    Co-authored-by: Matoran <4539516+Matoran@users.noreply.github.com>

--- devrank/create_graph.py
+++ devrank/create_graph.py
@@ -1,6 +1,8 @@
+import math
 import os
 import queue
 import random
+import sys
 import threading
 import time
 from datetime import datetime
@@ -9,7 +11,6 @@ from dotenv import load_dotenv
 from neo4j import GraphDatabase
 from neobolt.exceptions import ConstraintError
 from sgqlc.endpoint.http import HTTPEndpoint
-import math
 
 load_dotenv()
 
@@ -36,12 +37,6 @@ for i in range(10):
     headers.append({'Authorization': f'bearer {TOKEN}'})
 
 
-def init_queues(tx):
-    result = tx.run("MATCH(u:User) RETURN u")
-    for record in result:
-        users_already_done.add(record['u']['login'])
-
-
 def create_user(tx, login):
     tx.run(f"CREATE (a:User {{login: '{login}'}})")
 
@@ -56,55 +51,22 @@ def create_lang(tx, name):
 
 def create_lang_relation(tx, repo, lang, size):
     tx.run(f"""
-            MATCH (lang:Language{{name:'{lang}'}}),(repo:Repo{{name:'{repo}'}}) 
-            CREATE UNIQUE (repo)-[r:CONTAINS{{size:{size}}}]->(lang) return r
+            MATCH (lang:Language{{name:'{lang}'}}),(repo:Repo{{name:'{repo}'}})
+            MERGE (repo)-[r:CONTAINS]->(lang)
+                ON CREATE SET r.size = {size}
+                ON MATCH SET r.size = {size}
+            return r
         """)
 
 
 def create_relation(tx, repo, user, contributions_count):
     tx.run(f"""
-            MATCH (user:User{{login:'{user}'}}),(repo:Repo{{name:'{repo}'}}) 
-            CREATE UNIQUE (user)-[r:CONTRIBUTES{{count: {contributions_count}}}]->(repo) return r
+            MATCH (user:User{{login:'{user}'}}),(repo:Repo{{name:'{repo}'}})
+            MERGE (user)-[r:CONTRIBUTES]->(repo)
+                ON CREATE SET r.count = {contributions_count}
+                ON MATCH SET r.count = {contributions_count}
+            return r
         """)
-
-
-def build_knows_relation(tx):
-    print("Building knows relationships")
-    tx.run("""
-            CALL apoc.periodic.iterate("MATCH (u1:User)-[c1:CONTRIBUTES]->()<-[c2:CONTRIBUTES]-(u2:User) WHERE u1.login < u2.login RETURN *",
-            "MERGE (u1)-[r:KNOWS]->(u2) 
-            ON CREATE SET r.size = c2.count 
-            ON MATCH SET r.size = r.size + c2.count 
-            MERGE (u2)-[r2:KNOWS]->(u1) 
-            ON CREATE SET r2.size = c1.count 
-            ON MATCH SET r2.size = r2.size + c1.count", 
-            {batchSize:1000})
-            YIELD batches,total
-            RETURN batches,total
-           """)
-
-
-def build_codes_relation(tx):
-    tx.run("""
-            CALL apoc.periodic.iterate(
-            "MATCH (u1:User)-[r:CONTRIBUTES]->()-[:CONTAINS]->(l:Language) RETURN *",
-            "CALL apoc.merge.relationship(u1, 'CODES_IN', {}, {size: r.size}, l) YIELD rel RETURN rel", 
-            {batchSize:10000})
-           """)
-
-
-def compute_pagerank(tx):
-    print("calculating pagerank")
-    tx.run("""
-            CALL algo.pageRank('User','KNOWS',
-            {
-               iterations:20, 
-               dampingFactor:0.85, 
-               write: true,
-               writeProperty:'pagerank', 
-               weightProperty: 'size'
-             })
-           """)
 
 
 def safe_query(query, index=0):
@@ -295,9 +257,19 @@ class OrphanQueryThread(threading.Thread):
             index = (index + 1) % len(headers)
 
 
-driver.session().read_transaction(init_queues)
 start = time.time()
-query_for_user("maximelovino", 2)
+if len(headers) == 0:
+    print("You have to provide at least one GitHub Key")
+    exit(1)
+
+if len(sys.argv) < 3:
+    print("Usage: create_graph.py <username> <hops>")
+    exit(1)
+
+user = sys.argv[1]
+hops = int(sys.argv[2])
+print(f"Seed is {user} with {hops} hops")
+query_for_user(user, hops)
 while not users_to_process.empty():
     (u, hops) = users_to_process.get()
     query_for_user(u, hops)
@@ -318,23 +290,73 @@ for t in threads:
 
 print("...Done")
 end = time.time()
-print(f"Took {end - start} seconds")
-# query_for_user("Dawen18", 1)
-# query_for_user("Angorance")
-# query_for_user("stevenliatti")
-# query_for_user("Xcaliburne")
-# query_for_user("lekikou")
-# query_for_user("ProtectedVariable")
-# query_for_user("RaedAbr")
-# query_for_user("RalfJung")
-# query_for_user("selinux")
-# query_for_user("ry")
-# query_for_user("torvalds")
-# query_for_user("matoran")
-# query_for_user("anirul")
+print(f"Took {end - start} seconds for mining")
+print("Starting relations...")
 
-# driver.session().write_transaction(build_knows_relation)
-# driver.session().write_transaction(build_codes_relation)
-# driver.session().write_transaction(compute_pagerank)
+
+def delete_knows_relation(tx):
+    tx.run("""
+    Call apoc.periodic.iterate("Match (:User)-[r:KNOWS]->() return r", "delete r", {batchSize: 10000})
+           """)
+
+
+def build_knows_relation(tx):
+    print("Building knows relationships")
+    tx.run("""
+            cypher runtime=slotted CALL apoc.periodic.iterate("MATCH (u1:User)-[c1:CONTRIBUTES]->()<-[c2:CONTRIBUTES]-(u2:User)
+            WHERE u1.login < u2.login
+            RETURN *",
+            "MERGE (u1)-[r:KNOWS]->(u2) 
+                ON CREATE SET r.size = c2.count 
+                ON MATCH SET r.size = r.size + c2.count
+            MERGE (u2)-[r2:KNOWS]->(u1) 
+                ON CREATE SET r2.size = c1.count 
+                ON MATCH SET r2.size = r2.size + c1.count", 
+            {batchSize:100, iterateList:true})
+            YIELD batches,total RETURN batches,total
+           """)
+
+
+def delete_codes_relation(tx):
+    tx.run("""
+    Call apoc.periodic.iterate("Match (:User)-[r:CODES_IN]->() return r", "delete r", {batchSize: 10000})
+           """)
+
+
+def build_codes_relation(tx):
+    tx.run("""
+            CALL apoc.periodic.iterate(
+            "MATCH (u1:User)-[c:CONTRIBUTES]->()-[:CONTAINS]->(l:Language) RETURN *",
+            "MERGE (u1)-[r:CODES_IN]->(l) 
+                ON CREATE SET r.size = c.count 
+                ON MATCH SET r.size = r.size + c.count", 
+            {batchSize:10000})
+            YIELD batches, total RETURN batches, total
+           """)
+
+
+def compute_pagerank(tx):
+    print("calculating pagerank")
+    tx.run("""
+            CALL algo.pageRank('User','KNOWS',
+            {
+               iterations:20, 
+               dampingFactor:0.85, 
+               write: true,
+               writeProperty:'pagerank', 
+               weightProperty: 'size'
+             })
+           """)
+
+
+start = time.time()
+driver.session().write_transaction(delete_knows_relation)
+driver.session().write_transaction(delete_codes_relation)
+driver.session().write_transaction(build_knows_relation)
+driver.session().write_transaction(build_codes_relation)
+driver.session().write_transaction(compute_pagerank)
+print("...Done")
+end = time.time()
+print(f"Took {end - start} seconds for relations")
 
 driver.close()
